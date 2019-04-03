c699498a0b8dbdcd757b3c2366be75d4004ceee9
joshuatan33 <xiang2903@gmail.com>
2019-04-03 16:19:40 +0800

    completed EX 3

--- arrow-function.js
+++ arrow-function.js
@@ -1,21 +1,42 @@
+/////////////////// EX 1 /////////////////////////////
+
 function addAndMultiply(x, y, z) {
-    return ((x + y) * z)
-  }
-  
-  console.log(addAndMultiply(4,8,5)) // 60
+  return ((x + y) * z)
+}
 
-  const addAndMultiply2 = (x, y, z) => ((x + y) * z);
+console.log(addAndMultiply(4,8,5)) // 60
 
-  console.log('test this ' + addAndMultiply2(4, 8, 5))
+const addAndMultiply2 = (x, y, z) => ((x + y) * z);
 
-  ////////////////////////////////////////////////////////
+console.log('test this ' + addAndMultiply2(4, 8, 5))
 
-  function selfIntro(name, hobby, count) {
-    return "My name is " + name + ". I practice " + hobby + " " + count + " times a day."
-  }
-  
-  console.log(selfIntro('Shanqyeet', 'Kendama', '18')) // My name is Shanqyeet. I practice Kendama 18 times a day.
+////////////////////////////////////////////////////////
 
-  const selfIntro2 = (name, hobby, count) => (`My name is ${name}. I practice ${hobby} ${count} times a day.`);
+function selfIntro(name, hobby, count) {
+  return "My name is " + name + ". I practice " + hobby + " " + count + " times a day."
+}
 
-  console.log(selfIntro2('Shanqyeet', 'Kendama', '18'))
+console.log(selfIntro('Shanqyeet', 'Kendama', '18')) // My name is Shanqyeet. I practice Kendama 18 times a day.
+
+const selfIntro2 = (name, hobby, count) => (`My name is ${name}. I practice ${hobby} ${count} times a day.`);
+
+console.log(selfIntro2('Shanqyeet', 'Kendama', '18'))
+
+/////////////////// EX 2 ///////////////////////////////////////
+
+// tips: remove curly braces
+let squared = (x) => {
+  return x * x
+}
+
+let squared2 = (X) => (x * x)
+
+/////////////////// EX 3 /////////////////////////////////////////
+
+const prices = [1.00, 2.00, 3.00, 4.00]
+const gst = 0.06
+
+const pricesWithTax = prices.map(x => x * gst + x);    // Complete this with .map
+
+console.log(pricesWithTax) // you should get [1.06, 2.12, 3.18, 4.24]
+
